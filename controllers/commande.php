d1a33b947cc262cd84326d07f35c4bf41c8a3d5e
LAM David <davidlam024@gmail.com>
2022-11-11 15:06:53 +0100

    Dashboard client : validation commande en bdd

--- controllers/commande.php
+++ controllers/commande.php
@@ -3,9 +3,20 @@
 $commandeModel = new CommandeModel();
 $francoModel = new FrancoModel();
 
+/**
+ * On vérifie qu'un utilisateur est bien connecté pour valider le panier
+ * sinon on le redirige vers login
+ * TODO verifie si l'utilisateur connecté a bien le bon role
+ */
 $idUser = getUserId();
-$montant = montantPanier();
+if(!$idUser){
+    header('location: login');
+}
 
+/**
+ * On vérifie que le panier n'est pas vide
+ * sinon on redirige le client vers panier
+ */
 if(empty($_SESSION['panier'])){
     header('location: panier');
     exit;
@@ -13,22 +24,46 @@ if(empty($_SESSION['panier'])){
 
 /**
  * On redirige sur la page "panier" si le montant du panier n'atteint pas le franco
+ * TODO afficher un message pour prevenir que le montant n'atteint pas le franco
  */
 //if($montant<$francoModel->getFranco(1)){
 //    header('location:panier');
 //    exit;
 //}
 
-
+/**
+ * Validation de la commande
+ */
 if(!empty($_POST)){
     $dateLivraison = strip_tags(trim($_POST['dateLivraison']));
-    $idStatus = 3;
-//    $commandeModel -> addCommande($idUser,$montant,$dateLivraison,$idStatus);
-    $idCommande = $commandeModel->lastCommandeId($idUser);
+    $idArticle = $_POST['id_article'];
+    $label = $_POST['label_article'];
+    $quantite = $_POST['quantite' ];
 
-//    for($i=0 ; $i < count($_SESSION['panier'])  ; $i++ ){
-//        $commandeModel->addDetailsCommande($idCommande["max(id_commande)"],$_SESSION['panier'][$i]['id_article'],$_SESSION['panier'][$i]['prix'],$_SESSION['panier'][$i]['quantite']);
-//    }
+    for($i=0 ; $i<(count($_SESSION['panier'])) ; $i++) {
+        modifierQTeArticle($idArticle[$i], $quantite[$i]);
+    }
+    $montant = montantPanier();
+
+    /**
+     * Parametre status :
+     * 1) En attente
+     * 2) Validée
+     * 3) Refusée
+     */
+    $idStatus = 1;
+
+    /**
+     * Ajout en BDD dans 2 tables : commande et detailsCommande
+     *
+     */
+    $commandeModel -> addCommande($idUser,$montant,$dateLivraison,$idStatus);
+    $idCommande = $commandeModel->lastCommandeId($idUser);
+    for($i=0 ; $i < count($_SESSION['panier'])  ; $i++ ){
+        $commandeModel->addDetailsCommande($idCommande["max(id_commande)"],$_SESSION['panier'][$i]['id_article'],$_SESSION['panier'][$i]['prix'],$_SESSION['panier'][$i]['quantite']);
+    }
+
+
 
 //    TODO s'il n'y a pas d'érreur, on vide le panier'
 //    unset($_SESSION['panier']);
--- controllers/panier.php
+++ controllers/panier.php
@@ -12,19 +12,18 @@ $dateLivraison = dateFr($date->format('D d M Y'));
 $dateLivraison1 = addDayDate(1);
 $dateLivraison2 = addDayDate(2);
 
-$articleModel = new ArticleModel();
-if(!empty($_POST)){
-    dump('ok');
-    $idArticle = $_POST['id_article'];
-    $label = $_POST['label_article'];
-    $quantite = $_POST['quantite' ];
+//$articleModel = new ArticleModel();
+//if(!empty($_POST)){
+//
+//    $idArticle = $_POST['id_article'];
+//    $label = $_POST['label_article'];
+//    $quantite = $_POST['quantite' ];
+//
+//    for($i=0 ; $i<(count($articles)-1) ; $i++) {
+//        modifierQTeArticle($idArticle[$i], $quantite[$i]);
+//    }
+//}
 
-    for($i=0 ; $i<(count($articles)-1) ; $i++) {
-        modifierQTeArticle($idArticle[$i], $quantite[$i]);
-    }
-}
-
-//dump($articles);
 $title = "Votre panier";
 $template = "panier";
 include '../templates/base_user.phtml';
