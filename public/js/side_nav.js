2c2101caeaf37ff1d5dc0ec852a9c2508f59596a
LAM David <davidlam024@gmail.com>
2022-11-26 17:22:22 +0100

    Pages utilisateurs

--- public/js/side_nav.js
+++ public/js/side_nav.js
@@ -1,8 +1,7 @@
 const menuBurger = document.querySelector(".menu-burger");
 
 menuBurger.addEventListener('click',function(){
-    document.querySelector(".haut-navigation-liste").classList.remove('hide');
-    document.querySelector(".accueil").classList.add('hide');
+
 
 
 })
